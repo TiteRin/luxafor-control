6857f9954d00f0a01aaf1791f9dfad9c31afb8ba
Marine Lancien <marine.lancien@protonmail.com>
2025-09-15 14:13:16 +0200

    **feat: add Luxafor light control scripts**
    - Split light control into dedicated scripts (`red.ts`, `green.ts`, `off.ts`).
    - Updated `package.json` scripts for easier execution.

--- src/main.ts
+++
@@ -1,4 +0,0 @@
-import {device} from "luxafor-api";
-
-const luxafor = device();
-luxafor.color("red");
---
+++ src/red.ts
@@ -0,0 +1,5 @@
+import {device, devices} from "luxafor-api";
+
+const luxafor = device();
+// console.log(devices());
+luxafor.fadeTo("red");
