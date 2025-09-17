2aef0e374321d2b99c1ef3b8f0de021f13fe68a9
Marine Lancien <marine.lancien@protonmail.com>
2025-09-17 09:07:52 +0200

    **refactor: remove redundant Luxafor control scripts**
    - Deleted `red.ts`, `green.ts`, and `test.ts` as they are no longer needed.
    - Updated `package.json` to remove associated scripts.

--- src/red.ts
+++
@@ -1,5 +0,0 @@
-import {device, devices} from "luxafor-api";
-
-const luxafor = device();
-// console.log(devices());
-luxafor.fadeTo("red");
--- src/test.ts
+++
@@ -1,5 +0,0 @@
-import { setState } from "./main";
-
-setState("available");
-// setState("dnd");
-setState("off");
